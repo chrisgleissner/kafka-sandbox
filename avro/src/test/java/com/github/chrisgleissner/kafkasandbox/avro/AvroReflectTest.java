c5dadfa0d77b046ac9a6d575bb49a1a38868ddaf
Christian Gleissner <a-cgleissner@expedia.com>
2020-08-30 13:43:53 +0200

    Added avro test

--- avro/src/test/java/com/github/chrisgleissner/kafkasandbox/avro/AvroReflectTest.java
+++ avro/src/test/java/com/github/chrisgleissner/kafkasandbox/avro/AvroReflectTest.java
@@ -1,16 +1,19 @@
 package com.github.chrisgleissner.kafkasandbox.avro;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.Schema;
-import org.apache.avro.reflect.AvroIgnore;
-import org.apache.avro.reflect.AvroName;
-import org.apache.avro.reflect.ReflectData;
+import org.apache.avro.io.*;
+import org.apache.avro.reflect.*;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
@@ -28,24 +31,40 @@ public class AvroReflectTest {
                 "\"com.github.chrisgleissner.kafkasandbox.avro.AvroReflectTest$\""));
         assertThat(jsonSchema).contains("stringList");
         assertThat(jsonSchema).doesNotContain("sHidden");
-        System.out.println("JSON schema:\n" + schema.toString(true));
+        log.info("JSON schema:\n" + schema.toString(true));
     }
 
-    @Data
+    @Test
+    void canMarshallNullFields() throws IOException {
+        Bar bar = Bar.builder().s("1").build();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Encoder encoder = EncoderFactory.get().binaryEncoder(baos, null);
+        new ReflectDatumWriter<>(Bar.class).write(bar, encoder);
+        encoder.flush();
+        byte[] bytes = baos.toByteArray();
+        log.info("Marshalled {} to {} byte(s)", bar, bytes.length);
+
+        Decoder decoder = DecoderFactory.get().binaryDecoder(new ByteArrayInputStream(bytes), null);
+        Bar bar2 = new ReflectDatumReader<>(Bar.class).read(null, decoder);
+        assertThat(bar2.getS()).isEqualTo("1");
+    }
+
+    @Data @NoArgsConstructor @SuperBuilder
     public static class Foo {
-        private int i;
-        private Integer iWrapper;
+        @Nullable private int i;
+        @Nullable private Integer iWrapper;
     }
 
-    @Value
+    @Value @NoArgsConstructor(force = true) @AllArgsConstructor @SuperBuilder
     public static class Bar extends Foo {
-        byte[] byteArray;
         String s;
-        @AvroIgnore String sHidden;
-        BigDecimal[] bdArray;
+        @Nullable byte[] byteArray;
+        @Nullable @AvroIgnore String sHidden;
+        @Nullable BigDecimal[] bdArray;
         @AvroName("stringList")
-        List<String> sList;
-        List<Integer> iList;
-        Map<Long, String> map;
+        @Nullable List<String> sList;
+        @Nullable List<Integer> iList;
+        @Nullable Map<Long, String> map;
     }
 }
